12fa246609db1dc644a8eb00997cc9dc63ba4c95
Cengizhan Koser <cengizhankoserr@gmail.com>
2026-01-15 15:59:14 +0300

    had a data breach popup by chrome because of the public password on the test site. fixed this by adding chrome options to the driver in conftest.py file.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,11 +4,21 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from pages.login_page import LoginPage
 from data.test_data import TestData
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
 def driver():
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+    prefs = {
+        "credentials_enable_service": False,
+        "profile.password_manager_enabled": False,
+        "profile.password_manager_leak_detection": False
+    }
+    chrome_options.add_experimental_option("prefs", prefs)
+
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.maximize_window()
     yield driver
     driver.quit()
